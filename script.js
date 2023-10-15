0434ce6e84a8697316e45f61171f306d07e275f5
perblac <yhana.kromx@gmail.com>
2023-10-15 17:31:29 +0200

    comprueba si la pregunta ya se hizo

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const campoPregunta = document.getElementById("textoPregunta");
 let pregunta = "";
 let respuestaCorrecta = [];
 let enunciadoCorrecto = "";
+let preguntaId = "";//estaPregunta[0] + "-" + estaPregunta[1];
 let preguntasHechas = new Set();
 let aciertos = 0;
 let totalPreguntas = 0;
@@ -57,47 +58,67 @@ function calcularPregunta(numActor, tipoPregunta) {
 }
 
 function nuevaPregunta(){
-  let estaPregunta = generarPregunta();
-  while (preguntasHechas.has(nuevaPregunta)) {
+  let estaPregunta = [];//generarPregunta();  
+  let final = 0;
+  // console.log(preguntaId, preguntasHechas.has(preguntaId));
+  // while (preguntasHechas.has(preguntaId)) {
+  //   estaPregunta = generarPregunta();
+  //   preguntaId = estaPregunta[0] + "-" + estaPregunta[1];
+  // }
+  do {
     estaPregunta = generarPregunta();
+    preguntaId = estaPregunta[0] + "-" + estaPregunta[1];
+    console.log(preguntasHechas.has(preguntaId));
+    if (preguntasHechas.size >= actores.length * 3) final = 1;
+  } while (preguntasHechas.has(preguntaId) && !final);
+
+  if (final) {
+    campoPregunta.innerHTML = `¡Se han terminado todas las preguntas!`;
+  } else {
+    calcularPregunta(estaPregunta[0],estaPregunta[1]);
+    // preguntasHechas.add(preguntaId);
+    console.log(estaPregunta, preguntaId, preguntasHechas);
+    // totalPreguntas++;
+    campoPregunta.innerHTML = pregunta;
   }
-  calcularPregunta(estaPregunta[0],estaPregunta[1]);
-  preguntasHechas.add(estaPregunta);
-  totalPreguntas++;
-  campoPregunta.innerHTML = pregunta;
 }
 
 function generarPregunta() {
-  console.log(preguntasHechas);
+  //console.log(preguntasHechas);
   limpiarCampos();
 
-  let numeroActorEscogido = Math.round(Math.random() * actores.length);
-  let tipoPregunta = Math.round(Math.random() * 3);
+  let numeroActorEscogido = Math.floor(Math.random() * actores.length);
+  let tipoPregunta = Math.floor(Math.random() * 3) + 1; // tipos: 1 -> edad, 2 -> pais, 3 -> peli
   
   return [numeroActorEscogido,tipoPregunta];
 }
 
 function limpiarCampos() {  
   document.getElementById("respuesta").value = "";
-  let cabecera = `Su resultado: ${totalPreguntas} preguntas realizadas, ${aciertos} preguntas acertadas`;
+  let cabecera = `Su resultado: ${totalPreguntas} preguntas respondidas, ${aciertos} preguntas acertadas`;
   document.getElementById("resultadoHeader").innerHTML = cabecera;
   document.getElementById("resultadoTitle").innerHTML = "";
   document.getElementById("resultado").innerHTML = "";
 }
 
 function comprobarRespuesta() {
-  const respuestaDada = document.getElementById("respuesta").value;
-  console.log(respuestaDada);
-  let acertado = 0;
-  for (let acierto of respuestaCorrecta) {
-    if (respuestaDada == acierto) {
-      aciertos++;
-      acertado = 1;
+  if (!preguntasHechas.has(preguntaId)){
+    const respuestaDada = document.getElementById("respuesta").value;
+    console.log(respuestaDada);
+    let acertado = 0;
+    for (let acierto of respuestaCorrecta) {
+      if (respuestaDada == acierto) {
+        aciertos++;
+        acertado = 1;
+      }
     }
-  }
-  limpiarCampos();
-  document.getElementById("resultadoTitle").innerHTML = emote[acertado];
-  document.getElementById("resultado").innerHTML = enunciadoCorrecto;
+    limpiarCampos();
+    document.getElementById("resultadoTitle").innerHTML = emote[acertado];
+    document.getElementById("resultado").innerHTML = (acertado)?"¡Correcto! " + enunciadoCorrecto:"¡Qué pena! " + enunciadoCorrecto;
+    
+    preguntasHechas.add(preguntaId);
+    totalPreguntas++;
+  } else nuevaPregunta();
 }
 
 function init() {
